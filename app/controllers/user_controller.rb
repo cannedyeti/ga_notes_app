601896a197a0082ffb75de1ded1d1be72146c61d
dddotcom <dddotcom@gmail.com>
2017-04-24 12:45:28 -0700

    added ability to change password on user profile page; added logic user must enter password before any profile changes are made

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -23,9 +23,43 @@ class UserController < ApplicationController
   end
 
   def update
+    isChangingPassword = false
+    #password length validation?
     u = User.find(@current_user.id)
-    u.update(user_params)
+    #user is changing their password
+    if !user_params[:password_digest_new].blank?
+      if user_params[:password_digest_new] != user_params[:password_digest_check]
+        flash[:danger] = "Password Confirmation doesn't match New Password."
+        redirect_to "/profile"
+        return
+      end
+      isChangingPassword = true
+    end
+
+    params = {:password => user_params[:password_digest_current], :email => u.email}
+    user = User.authenticate(params)
+    if user
+      if user != -1
+        user.update(remove_unwanted_params(user_params, isChangingPassword))
+        flash[:success] = "Profile Updated"
+      else
+        flash[:danger] = "Current Password is invalid."
+      end
+    else
+      flash[:danger] = "Current Password is invalid."
+    end
     redirect_to "/profile"
+  end
+
+  def remove_unwanted_params(user_params, isChangingPassword)
+    if isChangingPassword
+      user_params[:password] = user_params[:password_digest_new]
+      puts "we are changing the password to " + user_params[:password]
+    end
+    user_params.delete(:password_digest_new)
+    user_params.delete(:password_digest_current)
+    user_params.delete(:password_digest_check)
+    return user_params
   end
 
   def destroy
@@ -40,6 +74,7 @@ class UserController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :name, :photo, :default_course_id, :location_id, :favorites => [])
+    params.require(:user).permit(:email, :password, :name, :photo, :default_course_id, :location_id,
+    :password_digest_current, :password_digest_new, :password_digest_check, :favorites => [])
   end
 end
